8eb1ba077a4e58662c937224aeeb835a73e887dc
solliegarcia <sollie@hawaii.edu>
2017-11-26 13:22:43 -1000

    Multiple bug fixes to how the footer sits at the bottom of the page.

--- app/imports/ui/layouts/user/index.js
+++ app/imports/ui/layouts/user/index.js
@@ -4,6 +4,7 @@ import './user-footer.html';
 import './user-footer.css';
 import './user-header.html';
 import './user-header.js';
+import './user-header.css';
 import './user-layout.css';
 import './user-layout.html';
 
