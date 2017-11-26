f8e767a827aa0cd003867caeb0f1e9d17b1e2496
solliegarcia <sollie@hawaii.edu>
2017-11-26 12:28:45 -1000

    Make footer sick to bottom of the page.

--- app/imports/ui/layouts/user/index.js
+++ app/imports/ui/layouts/user/index.js
@@ -1,7 +1,9 @@
 import './if-authorized.html';
 import './if-authorized.js';
 import './user-footer.html';
+import './user-footer.css';
 import './user-header.html';
 import './user-header.js';
 import './user-layout.css';
 import './user-layout.html';
+
