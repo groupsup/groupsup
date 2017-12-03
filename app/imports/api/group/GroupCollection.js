461a36e49be0f6c657420dacaa59c1b3ed44b346
Sollie Garcia <sollie@hawaii.edu>
2017-12-03 13:32:49 -1000

    Generate initial files.

---
+++ app/imports/api/group/GroupCollection.js
@@ -0,0 +1,7 @@
+import SimpleSchema from 'simpl-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
+import { Tracker } from 'meteor/tracker';
+
