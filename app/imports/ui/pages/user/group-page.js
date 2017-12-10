9ee4472d780b4b6c4405c0884c0172a47706fa81
N, Laranio <nlaranio@hawaii.edu>
2017-12-09 18:02:11 -1000

    add group; image, name, interest, elements.

--- app/imports/ui/pages/user/group-page.js
+++ app/imports/ui/pages/user/group-page.js
@@ -1,47 +1,21 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
-import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Groups } from '/imports/api/group/GroupCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
 
-const selectedInterestsKey = 'selectedInterests';
 
 Template.Group_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
-  this.subscribe(Profiles.getPublicationName());
+  this.subscribe(Groups.getPublicationName());
   this.messageFlags = new ReactiveDict();
-  this.messageFlags.set(selectedInterestsKey, undefined);
 });
 
 Template.Group_Page.helpers({
-  profiles() {
-    // Initialize selectedInterests to all of them if messageFlags is undefined.
-    if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
-      Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
-    }
-    // Find all profiles with the currently selected interests.
-    const allProfiles = Profiles.findAll();
-    const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
 
-  },
-
-  interests() {
-    return _.map(
-        Interests.findAll(),
-        function makeInterestObject(interest) {
-          return {
-            label: interest.name,
-            selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
-          };
-        });
-  },
 });
 
 Template.Group_Page.events({
-  'submit .filter-data-form'(event, instance) {
-    event.preventDefault();
-    const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
-    instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
-  },
+
 });
 
