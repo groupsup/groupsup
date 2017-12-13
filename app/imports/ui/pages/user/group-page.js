5efaec0ee3b38cc5bd8ecb568f246d7648f6ab9b
N, Laranio <nlaranio@hawaii.edu>
2017-12-12 14:11:28 -1000

    edit group route fix
    Fix routing bug to edit group.

--- app/imports/ui/pages/user/group-page.js
+++ app/imports/ui/pages/user/group-page.js
@@ -1,9 +1,9 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
 import { Groups } from '/imports/api/group/GroupCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
-
 
 Template.Group_Page.onCreated(function onCreated() {
   this.subscribe(Interests.getPublicationName());
@@ -14,6 +14,9 @@ Template.Group_Page.onCreated(function onCreated() {
 Template.Group_Page.helpers({
   isAdmin() { // setup admin check here.
     return true;
+  },
+  routeUserName() {
+    return FlowRouter.getParam('username');
   },
 });
 
